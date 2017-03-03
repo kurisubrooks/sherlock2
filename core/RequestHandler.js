ec6dc5c0a58025ea562ab8f727149fb9c9a4485b
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-04 05:26:11 +1100

    Better Data Handling & Logging

--- core/RequestHandler.js
+++ core/RequestHandler.js
@@ -18,8 +18,9 @@ class RequestHandler {
 
         const ip = req.ip.replace("::ffff:", "");
         const route = this.route(req.url);
-        const data = req.method === "POST" ? req.body : req.query;
-        const log = `${ip} /${route.path}/${route.endpoint} ${JSON.stringify(data)}`;
+        const params = req.method === "POST" ? req.body : req.query;
+        const data = Object.keys(params).length > 0 ? params : null;
+        const log = `${ip} /${route.path}/${route.endpoint} ${data ? JSON.stringify(data) : ""}`;
 
         res.set("Access-Control-Allow-Origin", "*");
         Logger.success("Router", log);
