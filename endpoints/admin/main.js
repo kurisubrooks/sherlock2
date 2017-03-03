669870747b2bb9f104471d14505efac864841d17
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-04 05:26:22 +1100

    Add basic stats

--- endpoints/admin/main.js
+++ endpoints/admin/main.js
@@ -4,13 +4,18 @@ class AdminEndpoint extends Endpoint {
     constructor() {
         super({
             name: "admin",
-            description: "idfk",
+            description: "administrative thing",
             route: "/api/"
         });
     }
 
     async run(req, res, data) {
-        return res.send({ ok: true, data });
+        return res.send({
+            ok: true,
+            node: process.version.replace("v", ""),
+            memory: `${Math.round(process.memoryUsage().heapUsed / 1024 / 1024)} MB`,
+            data
+        });
     }
 }
 
