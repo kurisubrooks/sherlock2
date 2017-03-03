d8a20639294616eae844ffa6da9214b3f2588b8d
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-04 02:58:45 +1100

    Rename Module to Endpoint

--- core/EndpointHandler.js
+++ core/EndpointHandler.js
@@ -5,35 +5,35 @@ const Util = require("./Util/Util");
 const Logger = require("./Util/Logger");
 const Collection = require("./Util/Collection");
 
-class ModuleLoader {
+class EndpointHandler {
     constructor(app) {
         this.app = app;
-        this.modules = new Collection();
+        this.endpoints = new Collection();
     }
 
     loadModules(dir) {
-        const modules = fs.readdirSync(path.join(__dirname, "..", dir));
+        const endpoints = fs.readdirSync(path.join(__dirname, "..", dir));
 
-        for (const item of modules) {
+        for (const item of endpoints) {
             const location = path.join(__dirname, "..", dir, item, "main.js");
             if (!fs.existsSync(location)) continue;
 
-            const Module = require(location);
-            const instance = new Module(this.app);
+            const Endpoint = require(location);
+            const instance = new Endpoint(this.app);
 
             if (instance.disabled) continue;
-            if (this.modules.has(instance.name)) throw new Error("Modules cannot have the same name");
+            if (this.endpoints.has(instance.name)) throw new Error("Endpoints cannot have the same name");
 
-            Logger.info("Loaded Module", Util.toUpper(instance.name));
-            this.modules.set(instance.name, instance);
+            Logger.info("Loaded", Util.toUpper(instance.name));
+            this.endpoints.set(instance.name, instance);
         }
     }
 
     runModule(endpoint, req, res, data) {
-        const module = this.modules.get(endpoint);
-        if (!module) return res.status(404).send({ ok: false, error: "Missing Endpoint" });
-        return module.run(req, res, data);
+        const route = this.endpoints.get(endpoint);
+        if (route) return res.status(404).send({ ok: false, error: "Missing Endpoint" });
+        return route.run(req, res, data);
     }
 }
 
-module.exports = ModuleLoader;
+module.exports = EndpointHandler;
