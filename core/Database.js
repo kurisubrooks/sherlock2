f9ea0e59a8ae82747787c14cd91a11efada60789
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-04 05:24:51 +1100

    Rename Username → Author, add Admin field to user

--- core/Database.js
+++ core/Database.js
@@ -20,7 +20,7 @@ class Database {
 
     static async newUser(data) {
         if (!data) return { ok: false, error: "Missing Data" };
-        if (!data.username) return { ok: false, error: "Missing Username" };
+        if (!data.author) return { ok: false, error: "Missing Author" };
         if (!data.application) return { ok: false, error: "Missing Application" };
         if (!data.permissions) data.permissions = {};
         if (!data.data) data.data = {};
@@ -29,21 +29,22 @@ class Database {
 
         await Database.models.User.create({
             token: token,
-            username: data.username,
+            author: data.author,
             application: data.application,
             permissions: JSON.stringify(data.permissions),
             data: JSON.stringify(data.data),
-            disabled: false
+            disabled: false,
+            admin: false
         });
 
-        return { ok: true, username: data.username, token: token };
+        return { ok: true, author: data.author, token: token };
     }
 
     static async checkToken(token) {
         const user = await Database.models.User.findOne({ where: { token } });
         return user
-            ? { ok: true, username: user.username }
-            : { ok: false, error: "Unable to find Token in Database" };
+            ? { ok: true, author: user.author }
+            : { ok: false, code: 500, error: "Unable to find Token in Database" };
     }
 
     static generateToken() {
--- core/Database/User.js
+++ core/Database/User.js
@@ -2,12 +2,14 @@ const Sequelize = require("sequelize");
 const Database = require("../Database");
 
 const Users = Database.db.define("users", {
+    disabled: Sequelize.BOOLEAN,
+    admin: Sequelize.BOOLEAN,
+
     token: Sequelize.STRING,
     username: Sequelize.STRING,
     application: Sequelize.STRING,
     permissions: Sequelize.STRING,
-    data: Sequelize.STRING,
-    disabled: Sequelize.BOOLEAN
+    data: Sequelize.STRING
 });
 
 Users.sync();
