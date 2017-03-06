0fe286ff38a89d532136a6262f696960b3214e7c
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-06 12:56:26 +1100

    Capital M(odels) for consistency

--- core/Database.js
+++ core/Database.js
@@ -14,13 +14,13 @@ class Database {
         return database;
     }
 
-    static get models() {
+    static get Models() {
         return { User: require("./Database/User") };
     }
 
     static async checkToken(token) {
         if (!token) return { ok: false, error: "Missing Token" };
-        const user = await Database.models.User.findOne({ where: { token } });
+        const user = await Database.Models.User.findOne({ where: { token } });
         return user
             ? { ok: true, username: user.username, token }
             : { ok: false, error: "Unable to Authenticate Token", input: token };
@@ -29,7 +29,7 @@ class Database {
     static async checkLogin(username, password) {
         if (!username) return { ok: false, error: "Missing Username" };
         if (!password) return { ok: false, error: "Missing Password" };
-        const user = await Database.models.User.findOne({ where: { username } });
+        const user = await Database.Models.User.findOne({ where: { username } });
         if (!user) return { ok: false, error: "User does not exist" };
         return bcrypt.compareSync(password, user.hash)
             ? { ok: true, token: user.token }
@@ -39,12 +39,12 @@ class Database {
     static async newUser(data) {
         if (!data) return { ok: false, error: "Missing Data" };
         if (!data.username) return { ok: false, error: "Missing Username" };
-        const check = await Database.models.User.findOne({ where: { username: data.username } });
+        const check = await Database.Models.User.findOne({ where: { username: data.username } });
         if (check) return { ok: false, error: "Username already exists" };
         const token = Database.generateToken();
         const hash = bcrypt.hashSync(data.password, 10);
 
-        await Database.models.User.create({
+        await Database.Models.User.create({
             token: token,
             username: data.username,
             password: hash,
@@ -60,7 +60,7 @@ class Database {
 
     static async deleteUser(token) {
         if (!token) return { ok: false, error: "Missing Token" };
-        const user = await Database.models.User.findOne({ where: { token } });
+        const user = await Database.Models.User.findOne({ where: { token } });
         if (!user) return { ok: false, error: "User does not exist" };
         await user.destroy();
         return { ok: true, username: user.username };
@@ -72,7 +72,7 @@ class Database {
         if (!data.name) return { ok: false, error: "Missing Name" };
         if (!data.description) return { ok: false, error: "Missing Description" };
         if (!data.url) return { ok: false, error: "Missing URL" };
-        const user = await Database.models.User.findOne({ where: { token: data.token } });
+        const user = await Database.Models.User.findOne({ where: { token: data.token } });
         if (!user) return { ok: false, error: "User does not exist" };
         const applications = JSON.parse(user.applications);
         applications.push({ name: data.name, description: data.description, url: data.url });
