ffcfa3071b65c2c2b8c1c97d5db2d26aadc98001
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-11 08:15:46 +1100

    Add RegKey Generator

---
+++ endpoints/admin/regkey.js
@@ -0,0 +1,27 @@
+const Endpoint = require("../../core/Endpoint");
+const Database = require("../../core/Database");
+
+class RegKeyGenerator extends Endpoint {
+    constructor() {
+        super({
+            name: "RegKey Generator",
+            description: "Handles RegKey Generation",
+            route: "/api/regkey",
+            method: "POST",
+            token: false
+            // admin: true
+        });
+    }
+
+    async run(req, res) {
+        if (!req.session || !req.session.token) return res.send({ ok: false, error: "Authentication Required" });
+        if (!req.session.admin) return res.send({ ok: false, error: "Forbidden" });
+
+        // Handle User Generation
+        const key = Database.newRegKey();
+        this.log("Generated New RegKey", "debug");
+        return res.send(key);
+    }
+}
+
+module.exports = RegKeyGenerator;
