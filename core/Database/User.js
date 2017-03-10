281f29321941b6cabf6d46dbb545b4c32036d5f0
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-10 20:15:17 +1100

    Tweak User Table

--- core/Database/User.js
+++ core/Database/User.js
@@ -2,14 +2,12 @@ const Sequelize = require("sequelize");
 const Database = require("../Database");
 
 const Users = Database.db.define("users", {
-    type: Sequelize.INTEGER,
-    disabled: Sequelize.BOOLEAN,
     admin: Sequelize.BOOLEAN,
-
+    disabled: Sequelize.BOOLEAN,
     token: Sequelize.STRING,
+    email: Sequelize.STRING,
     username: Sequelize.STRING,
     password: Sequelize.STRING,
-    applications: Sequelize.STRING,
     permissions: Sequelize.STRING,
     data: Sequelize.STRING
 });
