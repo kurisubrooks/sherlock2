f74113d37d2ff8cceefd7ed7964f33720e64557a
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-10 20:15:37 +1100

    Add RegKey Methods, Minor Fixes

--- core/Database.js
+++ core/Database.js
@@ -15,7 +15,22 @@ class Database {
     }
 
     static get Models() {
-        return { User: require("./Database/User") };
+        return {
+            User: require("./Database/User"),
+            RegKeys: require("./Database/RegistrationKeys")
+        };
+    }
+
+    static newRegKey() {
+        const key = crypto.randomBytes(Math.ceil(12 / 2)).toString("hex");
+        Database.Models.RegKeys.create({ key });
+        return { ok: true, key };
+    }
+
+    static async validateRegKey(key) {
+        return await Database.Models.RegKeys.findOne({ where: { key } })
+            ? { ok: true }
+            : { ok: false, error: "Invalid Registration Key" };
     }
 
     static async checkToken(token) {
@@ -37,11 +52,19 @@ class Database {
     static async checkLogin(username, password) {
         if (!username) return { ok: false, error: "Missing Username" };
         if (!password) return { ok: false, error: "Missing Password" };
+
+        // Check if username exists
         username = username.toLowerCase();
         const user = await Database.Models.User.findOne({ where: { username } });
         if (!user) return { ok: false, error: "Incorrect Credentials" };
+
+        // Compare passwords
         const auth = await bcrypt.compare(password, user.password);
+
+        // Dafauq is this for?
         if (auth && !user.admin) return { ok: false, error: "Administrators Only" };
+
+        // Return Results
         return auth
             ? { ok: true, token: user.token, admin: user.admin }
             : { ok: false, error: "Incorrect Credentials" };
@@ -49,48 +72,81 @@ class Database {
 
     static async newUser(data) {
         if (!data) return { ok: false, error: "Missing Data" };
-        if (!data.type) return { ok: false, error: "Missing Type" };
+        if (!data.auth) return { ok: false, error: "Missing Auth Key" };
+        if (!data.email) return { ok: false, error: "Missing Email" };
         if (!data.username) return { ok: false, error: "Missing Username" };
-        const check = await Database.Models.User.findOne({ where: { username: data.username } });
-        if (check) return { ok: false, error: "Username already exists" };
-        const token = Database.generateToken();
-        const hash = data.type === 1 ? bcrypt.hashSync(data.password, 10) : null;
+        if (!data.password) return { ok: false, error: "Missing Password" };
 
+        console.log("Data Exists");
+
+        // Check auth key
+        if (!Database.validateRegKey(data.auth)) return { ok: false, error: "Invalid Auth Key" };
+
+        console.log("Auth Key is OK");
+
+        // Check if user already exists
+        const check1 = await Database.Models.User.findOne({ where: { username: data.username } });
+        console.log(check1);
+        if (check1) return { ok: false, error: "Username already exists" };
+
+        console.log("User doesn't already exist, OK");
+
+        // Check if email is valid
+        if (!/(.+)@(.+){2,}\.(.+){2,}/.test(data.email)) return { ok: false, error: "Invalid Email" };
+
+        console.log("Email is VALID");
+
+        // Check if username is valid
+        if (!/^[a-zA-Z0-9]+$/.test(data.username)) return { ok: false, error: "Invalid Username" };
+
+        console.log("Username is VALID");
+
+        // Check if token already exists
+        const token = Database.generateToken();
+        const check2 = await Database.Models.User.findOne({ where: { token } });
+        console.log(check2);
+        if (check2) return { ok: false, error: "Internal Server Error" };
+
+        console.log("Generated token is OK");
+
+        // Hash password
+        const hash = bcrypt.hashSync(data.password, 10);
+
+        console.log("Hashed Password");
+
+        // Add user to db
         await Database.Models.User.create({
-            type: data.type,
+            admin: data.admin || false,
             token: token,
+            email: data.email,
             username: data.username,
             password: hash,
-            applications: JSON.stringify([]),
             permissions: JSON.stringify({}),
-            data: JSON.stringify({}),
-            disabled: false,
-            admin: data.admin || false
+            disabled: false
         });
 
+        console.log("Created user");
+
+        (await Database.Models.RegKeys.findOne({ where: { key: data.auth } })).destroy();
+
+        console.log("Deleted Auth Key");
+
+        // Return Data
         return { ok: true, username: data.username, token };
     }
 
     static async deleteUser(token) {
         if (!token) return { ok: false, error: "Missing Token" };
+
+        // Get user from db
         const user = await Database.Models.User.findOne({ where: { token } });
+
+        // Error if token doesn't belong to a user
         if (!user) return { ok: false, error: "User does not exist" };
+
+        // RIP OUT THEIR SPLEEN and serve it to them on a plate
         await user.destroy();
         return { ok: true, username: user.username };
-    }
-
-    static async newApplication(data) {
-        if (!data) return { ok: false, error: "Missing Data" };
-        if (!data.token) return { ok: false, error: "Missing Token" };
-        if (!data.name) return { ok: false, error: "Missing Name" };
-        if (!data.description) return { ok: false, error: "Missing Description" };
-        if (!data.url) return { ok: false, error: "Missing URL" };
-        const user = await Database.Models.User.findOne({ where: { token: data.token } });
-        if (!user) return { ok: false, error: "User does not exist" };
-        const applications = JSON.parse(user.applications);
-        applications.push({ name: data.name, description: data.description, url: data.url });
-        await user.update({ applications: JSON.stringify(applications) });
-        return { ok: true };
     }
 
     static generateToken() {
---
+++ core/Database/RegistrationKeys.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+const Database = require("../Database");
+
+const RegKeys = Database.db.define("regkeys", { key: Sequelize.STRING });
+
+RegKeys.sync();
+
+module.exports = RegKeys;
