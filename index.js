13101c7ea11901952a0ffd9d71bac6cc083350e2
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-12 16:15:22 +1100

    Tempoarily Remove HTTPS Code for Testing

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 
 global.Promise = require("bluebird");
 const server = require("express")();
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
 const Logger = require("./core/Util/Logger");
 const RequestHandler = require("./core/RequestHandler");
 
-const key = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.key"), "utf8");
-const cert = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.crt"), "utf8");
-const options = { key, cert };
+// const key = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.key"), "utf8");
+// const cert = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.crt"), "utf8");
+// const options = { key, cert };
 
 const http = require("http").createServer(server);
-const https = require("https").createServer(options, server);
+// const https = require("https").createServer(options, server);
 const request = new RequestHandler(server); // eslint-disable-line no-unused-vars
 
-http.listen(80, Logger.info("Core", "Listening on Port 80"));
-https.listen(443, Logger.info("Core", "Listening on Port 443"));
+http.listen(8080, Logger.info("Core", "Listening on Port 8080"));
+// https.listen(443, Logger.info("Core", "Listening on Port 443"));
