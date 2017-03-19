10e370350f0fcaa74acb21ce5d39c27e0b15bf5c
Kurisu Brooks <kurisubrooks@gmail.com>
2017-03-19 22:16:37 +1100

    Only listen on https (nginx)

--- index.js
+++ index.js
@@ -3,19 +3,17 @@
 
 global.Promise = require("bluebird");
 const server = require("express")();
-// const path = require("path");
-// const fs = require("fs");
+const path = require("path");
+const fs = require("fs");
 
 const Logger = require("./core/Util/Logger");
 const RequestHandler = require("./core/RequestHandler");
 
-// const key = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.key"), "utf8");
-// const cert = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.crt"), "utf8");
-// const options = { key, cert };
+const key = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.key"), "utf8");
+const cert = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.crt"), "utf8");
+const options = { key, cert };
 
-const http = require("http").createServer(server);
-// const https = require("https").createServer(options, server);
+const https = require("https").createServer(options, server);
 const request = new RequestHandler(server); // eslint-disable-line no-unused-vars
 
-http.listen(8080, Logger.info("Core", "Listening on Port 8080"));
-// https.listen(443, Logger.info("Core", "Listening on Port 443"));
+https.listen(9000, Logger.info("Core", "Listening on Port 9000"));
