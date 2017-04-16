8b4c6dcb4ba09282f9dfe45f1f3fcbe08659d740
Kurisu Brooks <kurisubrooks@gmail.com>
2017-04-16 15:39:08 +1000

    Add System, Remote Shutdown

--- endpoints/admin/system.js
+++ endpoints/admin/system.js
@@ -6,7 +6,6 @@ class SystemHandler extends Endpoint {
         super({
             name: "System",
             description: "Handles System/Server",
-            disabled: true,
             route: "/api/system",
             method: "POST",
             token: false,
@@ -16,13 +15,18 @@ class SystemHandler extends Endpoint {
     }
 
     async run(req, res, data) {
-        if (!req.session || !req.session.token) return res.send({ ok: false, error: "Authentication Required" });
+        if (!req.session.token) return res.send({ ok: false, error: "Authentication Required" });
         if (!req.session.admin) return res.send({ ok: false, error: "Forbidden" });
 
         if (data.action) {
             if (data.action === "shutdown") {
+                this.log("Shutdown triggered by administrator", "debug");
                 await res.send({ ok: true });
                 return setTimeout(() => process.exit(0), 500);
+            }
+
+            if (data.action === "reload") {
+                // reload endpoints
             }
         }
 
