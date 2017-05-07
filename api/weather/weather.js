8e3e97885f3a8dff00330fc7a7557c3545659f9a
Kurisu Brooks <kurisubrooks@gmail.com>
2017-05-07 14:15:36 +1000

    Migrate Weather to DarkSky, Icons Update

--- api/weather/weather.js
+++ api/weather/weather.js
@@ -1,6 +1,6 @@
 const Endpoint = require("../../core/Endpoint");
 const keys = require("../../keychain.json");
-const moment = require("moment");
+const moment = require("moment-timezone");
 const path = require("path");
 const fs = require("fs");
 
@@ -8,7 +8,7 @@ class Weather extends Endpoint {
     constructor() {
         super({
             name: "Weather",
-            description: "Get the Weather for Western Sydney",
+            description: "Get the Weather for Greater Western Sydney",
             route: "/api/weather",
             method: "all",
             token: false,
@@ -18,7 +18,7 @@ class Weather extends Endpoint {
                 enable: true,
                 interval: 5,
                 format: "json",
-                url: `http://api.wunderground.com/api/${keys.wunderground}/conditions/forecast10day/astronomy/q/Australia/Penrith.json`
+                url: `https://api.darksky.net/forecast/${keys.darksky}/-33.75,150.70?units=si`
             }
         });
     }
@@ -26,129 +26,89 @@ class Weather extends Endpoint {
     run(req, res) {
         const store = fs.readFileSync(path.join(__dirname, "..", "..", "storage", `${this.name}.json`));
         const weather = JSON.parse(store).data;
-
-        if (!weather.current_observation) {
-            res.status(500).send({ ok: false, error: "Internal Server Error" });
-            return this.error("Missing Data");
-        }
-
-        const icon_code = this.icon(weather.current_observation.icon, moment.unix(weather.current_observation.local_epoch).format("HHMM"), weather.sun_phase);
-
+        const icon = this.icon(weather.currently.icon);
         const result = {
             ok: true,
-            location: {
-                full: weather.current_observation.display_location.full,
-                city: weather.current_observation.display_location.city,
-                state: weather.current_observation.display_location.state,
-                country: weather.current_observation.display_location.country_iso3166,
-                lat: weather.current_observation.display_location.latitude,
-                long: weather.current_observation.display_location.longitude,
-                time: weather.current_observation.local_epoch,
-                timezone: weather.current_observation.local_tz_long,
-                offset: weather.current_observation.local_tz_offset
-            },
             weather: {
-                icon: icon_code,
-                image: `https://api.kurisubrooks.com/static/weather/icons_v2/${icon_code}.png`,
-                condition: weather.current_observation.weather,
-                temperature: Math.round(Number(weather.current_observation.temp_c)),
-                feels_like: Math.round(Number(weather.current_observation.feelslike_c)),
-                dewpoint: Number(weather.current_observation.dewpoint_c),
-                humidity: weather.current_observation.relative_humidity,
-                pressure: `${weather.current_observation.pressure_mb} mBar`,
-                visibility: `${weather.current_observation.visibility_km} km`,
-                UV: Number(weather.current_observation.UV),
-                wind: {
-                    chill: weather.current_observation.windchill_c,
-                    direction: weather.current_observation.wind_dir,
-                    degrees: Number(weather.current_observation.wind_degrees),
-                    gust: `${weather.current_observation.wind_gust_kph} km/h`,
-                    kph: `${weather.current_observation.wind_kph} km/h`
-                },
-                precipitation: {
-                    hour: `${weather.current_observation.precip_1hr_metric} mm`,
-                    today: `${weather.current_observation.precip_today_metric} mm`
-                }
+                icon,
+                time: moment.unix(weather.currently.time).tz(weather.timezone).format(),
+                image: `https://api.kurisubrooks.com/static/weather/icons_v2/${icon}.png`,
+                condition: weather.currently.summary,
+                temperature: Math.round(weather.currently.temperature),
+                feels_like: Math.round(weather.currently.apparentTemperature),
+                dewpoint: Math.round(weather.currently.dewPoint),
+                humidity: `${Math.round(weather.currently.humidity * 100)}%`,
+                pressure: `${Math.round(weather.currently.pressure)} mBar`
             },
             forecast: []
         };
 
-        for (const object of weather.forecast.simpleforecast.forecastday) {
+        for (const forecast of weather.daily.data) {
+            const date = moment.unix(forecast.time).tz(weather.timezone);
+            const icon = this.icon(forecast.icon);
+
             result.forecast.push({
                 date: {
-                    day: object.date.day,
-                    month: object.date.month,
-                    year: object.date.year,
-                    total: object.date.yday,
+                    time: forecast.time,
+                    timezone: weather.timezone,
+
+                    day: date.format("D"),
+                    month: date.format("M"),
+                    year: date.format("YYYY"),
+                    total: date.format("DDD"),
+
                     display: {
-                        day: object.date.weekday,
-                        day_short: object.date.weekday_short,
-                        month: object.date.monthname,
-                        month_short: object.date.monthname_short
+                        day: date.format("dddd"),
+                        day_short: date.format("ddd"),
+                        month: date.format("MMMM"),
+                        month_short: date.format("MMM")
                     },
-                    time: object.date.epoch,
-                    timezone: object.date.tz_long
+
+                    sunrise: moment.unix(forecast.sunriseTime).tz(weather.timezone).format(),
+                    sunset: moment.unix(forecast.sunsetTime).tz(weather.timezone).format()
                 },
-                icon: this.icon(object.icon),
-                image: `https://api.kurisubrooks.com/static/weather/icons_v2/${this.icon(object.icon)}.png`,
-                condition: object.conditions,
-                high: Math.round(Number(object.high.celsius)),
-                low: Math.round(Number(object.low.celsius)),
-                humidity: `${object.avehumidity}%`,
-                rain_chance: `${object.pop}%`,
-                rainfall: `${object.qpf_allday.mm} mm`,
-                snowfall: `${object.snow_allday.cm} cm`,
-                wind: {
-                    max: `${object.maxwind.kph} km/h`,
-                    average: `${object.avewind.kph} km/h`,
-                    direction: object.avewind.dir,
-                    degrees: Number(object.avewind.degrees)
-                }
+                icon,
+                time: date.format(),
+                image: `https://api.kurisubrooks.com/static/weather/icons_v2/${icon}.png`,
+                condition: forecast.summary,
+                temperature: {
+                    high: forecast.temperatureMax,
+                    low: forecast.temperatureMin
+                },
+                feels_like: {
+                    high: forecast.apparentTemperatureMax,
+                    low: forecast.apparentTemperatureMin
+                },
+                humidity: `${Math.round(forecast.humidity * 100)}%`,
+                pressure: `${Math.round(forecast.pressure)} mBar`,
+                rain_chance: `${Math.round(forecast.precipProbability * 100)}%`
             });
         }
 
         return res.send(result);
     }
 
-    pad(num) {
-        return String(num).length === 1 ? `0${String(num)}` : String(num);
-    }
-
-    icon(condition, now, phases) {
-        let sunrise, sunset, day;
-
-        if (now && phases) {
-            sunrise = `${this.pad(phases.sunrise.hour)}${this.pad(phases.sunrise.minute)}`;
-            sunset = `${this.pad(phases.sunset.hour)}${this.pad(phases.sunset.minute)}`;
-            day = now >= sunrise && now <= sunset;
-        } else {
-            day = true;
-        }
-
+    icon(condition) {
         const icons = {
-            "chanceflurries": "snow",
-            "chancerain": "rain",
-            "chancesleat": "snow",
-            "chancesnow": "snow",
-            "chancetstorms": "storm",
-            "clear": day ? "day" : "night",
-            "cloudy": "cloudy",
-            "flurries": "snow",
-            "fog": "particles",
-            "hazy": "particles",
-            "mostlycloudy": day ? "day_mostlycloudy" : "night_mostlycloudy",
-            "mostlysunny": "day_mostlyclear",
-            "partlycloudy": day ? "day_partlycloudy" : "night_partlycloudy",
-            "partlysunny": "day_mostlycloudy",
+            "clear-day": "day",
+            "clear-night": "night",
             "rain": "rain",
-            "sleat": "snow",
             "snow": "snow",
-            "sunny": "day",
-            "tstorms": "storm",
+            "sleet": "snow",
+            "wind": "wind",
+            "fog": "particles",
+            "cloudy": "cloudy",
+            "partly-cloudy-day": "day_partlycloudy",
+            "partly-cloudy-night": "night_partlycloudy",
+            "hail": "snow",
+            "thunderstorm": "storm",
+            "tornado": "wind",
             "unknown": "unknown"
         };
 
-        return condition && condition !== "" && icons[condition] ? icons[condition] : icons.unknown;
+        return condition && condition !== "" && icons[condition]
+            ? icons[condition]
+            : icons.unknown;
     }
 }
 
