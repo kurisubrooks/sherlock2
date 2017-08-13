0e999397267ac033e9522d6c01b90da9f88e3992
Kurisu Brooks <kurisubrooks@gmail.com>
2017-08-14 08:29:55 +1000

    Support for Multiple Retrievers & Data Integrity Checks

--- core/DataRetriever.js
+++ core/DataRetriever.js
@@ -7,28 +7,65 @@ class DataRetriever {
     constructor(instance) {
         this.instance = instance;
         this.storage = path.join(__dirname, "..", "storage");
+
         if (!fs.existsSync(this.storage)) fs.mkdirSync(this.storage);
     }
 
     start() {
-        if (!this.instance.retriever.enable) return false;
-        if (!this.instance.retriever.interval) throw new Error("Data Retriever must have a set interval (in minutes)!");
-        if (!this.instance.retriever.format) throw new Error("Data Retriever must have a format");
-        if (!this.instance.retriever.url) throw new Error("Data Retriever must have a url");
+        if (Array.isArray(this.instance.retriever)) {
+            for (var index = 0; index < this.instance.retriever.length; index++) {
+                const data = this.instance.retriever[index];
 
-        this.handle(this.instance.retriever);
-        return setInterval(() => this.handle(this.instance.retriever), this.instance.retriever.interval * 60 * 1000);
+                if (data.enable && data.enable === false) continue;
+                if (!data.name || !data.interval || !data.format || !data.url) {
+                    Logger.error("Missing Name, Interval, Format or URL Parameters");
+                    continue;
+                }
+
+                this.handle(data);
+                setInterval(() => this.handle(data), data.interval * 60 * 1000);
+            }
+        } else {
+            const data = this.instance.retriever;
+
+            if (data.enable && data.enable === false) return false;
+            if (!data.name) throw new Error("Data Retriever must have a save file name");
+            if (!data.interval) throw new Error("Data Retriever must have a set interval (in minutes)!");
+            if (!data.format) throw new Error("Data Retriever must have a format");
+            if (!data.url) throw new Error("Data Retriever must have a url");
+
+            this.handle(data);
+            setInterval(() => this.handle(data), data.interval * 60 * 1000);
+        }
+
+        return false;
     }
 
     async handle(options) {
-        const response = await request({
+        let response = await request({
             headers: { "User-Agent": "Mozilla/5.0" },
             uri: options.url,
             json: options.format === "json"
         }).catch(error => this.error(error));
 
         if (!response) return false;
-        return fs.writeFileSync(path.join(this.storage, `${this.instance.name}.json`), JSON.stringify({ ok: true, data: response }));
+
+        response = typeof response === "string" ? JSON.parse(response) : response;
+
+        if (this.instance.verify) {
+            const result = this.instance.verify(response);
+
+            if (result === 0) {
+                Logger.error("Data Retriever", `Data returned from ${this.instance.name} failed quality checking.`);
+                return false;
+            }
+        }
+
+        if (options.format === "json") {
+            return fs.writeFileSync(path.join(this.storage, `${options.name}.json`), JSON.stringify({ ok: true, data: response }));
+        }
+
+        return fs.writeFileSync(path.join(this.storage, `${options.name}.${options.format}`), response);
     }
 
     error(message) {
