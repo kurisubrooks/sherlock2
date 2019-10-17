5e25ef6c9f35a7d8f48f58792cadc29f25e8e42c
Kurisu Brooks <kurisubrooks@gmail.com>
2019-10-17 21:59:38 +1100

    SSL back to HTTP
    nginx handles it so...

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
 // Sherlock Core v2
 // by @kurisubrooks
 
-global.Promise = require("bluebird");
-const server = require("express")();
-const path = require("path");
-const fs = require("fs");
+global.Promise = require('bluebird');
+const server = require('express')();
 
-const Logger = require("./core/Util/Logger");
-const RequestHandler = require("./core/RequestHandler");
+const Logger = require('./core/Util/Logger');
+const RequestHandler = require('./core/RequestHandler');
 
-const key = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.key"), "utf8");
-const cert = fs.readFileSync(path.join(__dirname, "secure", "api.kurisubrooks.com.crt"), "utf8");
-const options = { key, cert };
-
-const https = require("https").createServer(options, server);
+const http = require('http').createServer(server);
 const request = new RequestHandler(server); // eslint-disable-line no-unused-vars
 
-https.listen(9000, Logger.info("Core", "Listening on Port 9000"));
+http.listen(9000, Logger.info('Core', 'Listening on Port 9000'));
